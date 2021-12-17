cb64a6df5679006efcc7d279e8e1fbc261cefc11
LucasJeanpierre <61747163+LucasJeanpierre@users.noreply.github.com>
2021-12-17 11:08:07 +0100

    add comment and readme informations

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
 # AnimationController
 CSS Animation controller in JS
+
+## Installation
+ - The only needed file is the animationController.js, set it up in your project and you are ready to use it.
+
+
+## Features
+ - Start/Pause/Stop CSS animation easily with JavaScript
+
+## Quick Start
+
+### Setup the CSS
+
+ - You will need a small change in your CSS file before to use the AnimationContoller.
+You need to seperate the animation attribute from the initial CSS attriubtes in a new CSS class.
+
+example  :
+
+ - Before 
+    ```CSS
+    .custom-circle {
+        width: 30px;
+        height: 30px;
+        border-radius: 100%;
+        -webkit-animation: 2s linear infinite custom-circle;
+        animation: 2s linear infinite custom-circle;
+    }
+    ```
+
+- After
+
+    ```CSS
+    .custom-circle {
+        width: 30px;
+        height: 30px;
+        border-radius: 100%;
+    }
+
+    .custom-circle-animation {
+        -webkit-animation: 2s linear infinite custom-circle;
+        animation: 2s linear infinite custom-circle;
+    }
+    ```
+
+### Setup the HTML
+
+
+ - Start with include the script to your page
+    ```HTML
+        <script src="animationController.js"></script>
+    ```
+
+ - The only html setup you need is to precise the new CSS class you have just create in your HTML element 
+
+    example :
+
+    ```HTML
+        <div class="custom-circle bg-info" data-animationClass="custom-circle-animation" id="animationExample1" role="status"></div>
+    ```
+
+ - If you want to call the function from the DOM element you need to init the controller at the end of your page
+
+    ```HTML
+        <script>
+            AnimationController.init();
+        </script>
+    ```
+
+## Basic Example
+
+ - Start the animation of an element with its ID : 
+    ```javascript
+        AnimationController.startAnimation('elementId');
+    ```
+
+- Start the animation from the DOM element
+    ```javascript
+        element.startAnimation();
+    ```
+
+- Start the animation from the DOM element 3 time in a row
+    ```javascript
+        element.startAnimation(3);
+    ```
+
+- Start a global animation with its name
+    ```javascript
+        AnimationController.startGlobalAnimation('global-animation', 0.2)
+    ```
--- animationController.js
+++ animationController.js
@@ -1,13 +1,34 @@
+/**
+ * Author : Lucas JEANPIERRE
+ * 
+ * AnimationController object
+ * 
+ */
+
+
 const AnimationController = {
     animationInterval: Array(),
     animationTimeOut: Array(Array()),
     isAnimationLooping: Array(),
     isAnimationRunning: Array(),
 
+    /**
+     * AnimationController.startAnimation
+     * 
+     * @param string elementId
+     * @param int count default : 0
+     * 
+     * start the animation of the wanted element
+     * 
+     * no count attribute given -> infinite loop
+     * 
+     * example : AnimationController.startAnimation('myElementId', 3)
+     * 
+     */
     startAnimation: function(elementId, count = 0) {
         element = document.getElementById(elementId);
         if (element != null) {
-            var animationClass = element.getAttribute('animationClass');
+            var animationClass = element.getAttribute('data-animationClass');
             if (animationClass != null) {
                 element.classList.add(animationClass)
                 if (element.style.animationPlayState == 'paused') element.style.animationPlayState = 'running';
@@ -17,45 +38,76 @@ const AnimationController = {
                     }, parseFloat(getComputedStyle(element).animationDuration) * 1000 * parseInt(count))
                 }
             } else {
-                console.warn('AnimationController : The element do not have animationClass attribute');
+                console.warn('AnimationController : The element do not have data-animationClass attribute');
             }
         } else {
             console.warn('AnimationController : No element found');
         }
     },
 
+    /**
+     * AnimationController.stopAnimation
+     * 
+     * @param string elementId
+     * 
+     * stop the animation of the wanted element and set the element at its orginial state
+     * 
+     * example : AnimationController.stopAnimation('myElementId')
+     */
     stopAnimation: function(elementId) {
         element = document.getElementById(elementId);
         if (element != null) {
-            var animationClass = element.getAttribute('animationClass');
+            var animationClass = element.getAttribute('data-animationClass');
             if (animationClass != null) {
                 if (element.style.animationPlayState == 'running') element.style.animationPlayState = 'paused';
                 element.classList.remove(animationClass)
             } else {
-                console.warn('AnimationController : The element do not have animationClass attribute');
+                console.warn('AnimationController : The element do not have data-animationClass attribute');
             }
         } else {
             console.warn('AnimationController : No element found');
         }
     },
 
+    /**
+     * AnimationController.pauseAnimation
+     * 
+     * @param string elementId
+     * 
+     * pause the animation at its current state
+     * 
+     * example : AnimationController.pauseAnimation('myElementId')
+     */
     pauseAnimation: function(elementId) {
         element = document.getElementById(elementId);
         if (element != null) {
-            var animationClass = element.getAttribute('animationClass');
+            var animationClass = element.getAttribute('data-animationClass');
             if (animationClass != null) {
                 element.style.animationPlayState = 'paused';
             } else {
-                console.warn('AnimationController : The element do not have animationClass attribute');
+                console.warn('AnimationController : The element do not have data-animationClass attribute');
             }
         } else {
             console.warn('AnimationController : No element found');
         }
     },
 
+    /**
+     * AnimationController.startAnimationByElement
+     * 
+     * @param DOM element
+     * @param int count default : 0
+     * 
+     * start the animation of the given element
+     * 
+     * no count attribute given -> infinite loop
+     * 
+     * example : AnimationController.startAnimation(myElement, 3)
+     * 
+     */
     startAnimationByElement: function(element, count = 0) {
         if (element != null) {
-            var animationClass = element.getAttribute('animationClass');
+            var animationClass = element.getAttribute('data-animationClass');
             if (animationClass != null) {
                 element.classList.add(animationClass)
                 if (element.style.animationPlayState == 'paused') element.style.animationPlayState = 'running';
@@ -65,42 +117,69 @@ const AnimationController = {
                     }, parseFloat(getComputedStyle(element).animationDuration) * 1000 * parseInt(count))
                 }
             } else {
-                console.warn('AnimationController : The element do not have animationClass attribute');
+                console.warn('AnimationController : The element do not have data-animationClass attribute');
             }
         } else {
             console.warn('AnimationController : No element found');
         }
     },
 
+    /**
+     * AnimationController.stopAnimationByElement
+     * 
+     * @param DOM element
+     * 
+     * stop the animation of the given element and set the element at its orginial state
+     * 
+     * example : AnimationController.stopAnimation(myElement)
+     */
     stopAnimationByElement: function(element) {
         if (element != null) {
-            var animationClass = element.getAttribute('animationClass');
+            var animationClass = element.getAttribute('data-animationClass');
             if (animationClass != null) {
                 if (element.style.animationPlayState == 'running') element.style.animationPlayState = 'paused';
                 element.classList.remove(animationClass)
             } else {
-                console.warn('AnimationController : The element do not have animationClass attribute');
+                console.warn('AnimationController : The element do not have data-animationClass attribute');
             }
         } else {
             console.warn('AnimationController : No element found');
         }
     },
 
+    /**
+     * AnimationController.pauseAnimationByElement
+     * 
+     * @param DOM element
+     * 
+     * pause the animation at its current state
+     * 
+     * example : AnimationController.pauseAnimation(myElement)
+     */
     pauseAnimationByElement: function(element) {
         if (element != null) {
-            var animationClass = element.getAttribute('animationClass');
+            var animationClass = element.getAttribute('data-animationClass');
             if (animationClass != null) {
                 element.style.animationPlayState = 'paused';
             } else {
-                console.warn('AnimationController : The element do not have animationClass attribute');
+                console.warn('AnimationController : The element do not have data-animationClass attribute');
             }
         } else {
             console.warn('AnimationController : No element found');
         }
     },
 
+    /**
+     * AnimationController.startElementOfGlobalAnimation
+     * 
+     * @param DOM element
+     * 
+     * Start the animation af an element of the globalAnimation
+     * 
+     * example : AnimationController.startElementOfGlobalAnimation(myElement)
+     */
     startElementOfGlobalAnimation: function(element) {
-        var animationClass = element.getAttribute('animationClass');
+        var animationClass = element.getAttribute('data-animationClass');
         if (animationClass != null) {
             if (element.style.animationPlayState == 'paused') element.style.animationPlayState = 'running';
             if (!element.classList.contains(animationClass)) {
@@ -112,32 +191,55 @@ const AnimationController = {
                 element.classList.toggle(animationClass)
             }
         } else {
-            console.warn('AnimationController : The element do not have animationClass attribute');
+            console.warn('AnimationController : The element do not have data-animationClass attribute');
         }
     },
 
+    /**
+     * AnimationController.stopElementOfGlobalAnimation
+     * 
+     * @param DOM element
+     * 
+     * Stop the animation af an element of the globalAnimation
+     * 
+     * example : AnimationController.stopElementOfGlobalAnimation(myElement)
+     */
     stopElementOfGlobalAnimation: function(element) {
-        var animationClass = element.getAttribute('animationClass');
+        var animationClass = element.getAttribute('data-animationClass');
         if (animationClass != null) {
             if (element.style.animationPlayState == 'running') element.style.animationPlayState = 'paused';
             if (element.classList.contains(animationClass)) {
                 element.classList.remove(animationClass)
             }
         } else {
-            console.warn('AnimationController : The element do not have animationClass attribute');
+            console.warn('AnimationController : The element do not have data-animationClass attribute');
         }
     },
 
+    /**
+     * AnimationController.startGlobalAnimation
+     * 
+     * @param string globalAnimationName
+     * @param float elementDelay
+     * 
+     * start the wanted global animation
+     * 
+     * elementDelay -> delay between each animated object in seconds
+     * 
+     * example : AnimationController.startGlobalAnimation('myGlobalAnimationName', 1.5)
+     * 
+     * 
+     */
     startGlobalAnimation: function(globalAnimationName, elementDelay) {
         if ((this.isAnimationRunning[globalAnimationName] == false) || (this.isAnimationRunning[globalAnimationName] == undefined)) {
             this.isAnimationRunning[globalAnimationName] = true;
             this.animationTimeOut[globalAnimationName] = Array();
-            document.querySelectorAll('[globalAnimationName="' + globalAnimationName + '"]').forEach(element => {
-                if (this.animationTimeOut[globalAnimationName][element.getAttribute('globalAnimationOrder')] != null) clearTimeout(this.animationTimeOut[globalAnimationName][element.getAttribute('globalAnimationOrder')]);
-                this.animationTimeOut[globalAnimationName][element.getAttribute('globalAnimationOrder')] = setTimeout(() => {
+            document.querySelectorAll('[data-globalAnimationName="' + globalAnimationName + '"]').forEach(element => {
+                if (this.animationTimeOut[globalAnimationName][element.getAttribute('data-globalAnimationOrder')] != null) clearTimeout(this.animationTimeOut[globalAnimationName][element.getAttribute('data-globalAnimationOrder')]);
+                this.animationTimeOut[globalAnimationName][element.getAttribute('data-globalAnimationOrder')] = setTimeout(() => {
                     this.startElementOfGlobalAnimation(element);
                     this.isAnimationRunning[globalAnimationName] = false;
-                }, parseInt(element.getAttribute('globalAnimationOrder')) * elementDelay * 1000);
+                }, parseInt(element.getAttribute('data-globalAnimationOrder')) * elementDelay * 1000);
 
             })
         } else {
@@ -146,6 +248,20 @@ const AnimationController = {
     },
 
     /**
+     * AnimationController.startGlobalAnimationLoop
+     * 
+     * @param string globalAnimationName
+     * @param float elementDelay
+     * @param float loopDuration
+     * 
+     * loop the wanted global animation 
+     * 
+     * elementDelay -> delay between each animated object in seconds
+     * 
+     * loopDuration -> delay between each start of the globalAnimation
+     * 
+     * example : AnimationController.startGlobalAnimationLoop('myGlobalAnimationName', 1.5)
+     * 
      * !! loopDuration must be smaller than the animationDuration
      */
     startGlobalAnimationLoop: function(globalAnimationName, elementDelay, loopDuration) {
@@ -164,28 +280,54 @@ const AnimationController = {
         }
     },
 
+    /**
+     * AnimationController.stopGlobalAnimationLoop
+     * 
+     * @param string globalAnimationName
+     * 
+     * stop the wanted globalAnimation imediately
+     * 
+     * example : AnimationController.stopGlobalAnimationLoop('myGlobalAnimationName')
+     * 
+     */
     stopGlobalAnimationLoop: function(globalAnimationName) {
-        document.querySelectorAll('[globalAnimationName="' + globalAnimationName + '"]').forEach(element => {
+        document.querySelectorAll('[data-globalAnimationName="' + globalAnimationName + '"]').forEach(element => {
             this.stopElementOfGlobalAnimation(element);
-            clearTimeout(this.animationTimeOut[globalAnimationName][element.getAttribute('globalAnimationOrder')])
+            clearTimeout(this.animationTimeOut[globalAnimationName][element.getAttribute('data-globalAnimationOrder')])
         })
         clearInterval(AnimationController.animationInterval[globalAnimationName]);
         this.isAnimationLooping[globalAnimationName] = false;
     },
 
+    /**
+     * AnimationController.endGlobalAnimationLoop
+     * 
+     * @param string globalAnimationName
+     * 
+     * stop the looping of the wanted globalAnimation
+     * 
+     * example : AnimationController.endGlobalAnimationLoop('myGlobalAnimationName')
+     * 
+     */
     endGlobalAnimationLoop: function(globalAnimationName) {
         clearInterval(AnimationController.animationInterval[globalAnimationName]);
         this.isAnimationLooping[globalAnimationName] = false;
     },
 
 
+    /**
+     * AnimationController.init
+     * 
+     * @param none
+     * 
+     * attach start/stop/pause function to dom element with the data-animationClass attribute
+     */
     init: function() {
-        document.querySelectorAll("[animationClass]").forEach(element => {
+        document.querySelectorAll("[data-animationClass]").forEach(element => {
             element.startAnimation = function(count = 0) { AnimationController.startAnimationByElement(element, count) };
             element.stopAnimation = function() { AnimationController.stopAnimationByElement(element) };
             element.pauseAnimation = function() { AnimationController.pauseAnimationByElement(element) };
         })
     }
-
 
 }
